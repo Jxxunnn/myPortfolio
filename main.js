f4cd1326070ab4ddcda478d3418e304980c048a7
Jxxunnn <86228307+Jxxunnn@users.noreply.github.com>
2021-12-29 13:22:50 +0900

    portfolio updated

--- main.js
+++ main.js
@@ -103,3 +103,50 @@ workButtonContainer.addEventListener("click", (e) => {
     projectContainer.classList.remove("anime--out");
   }, 500);
 });
+
+// IntersectionObserver
+// 1. 모든 섹션 요소들과 메뉴 아이템을 가지고 온다
+// 2. IntersectionObserver를 이용해서 모든 섹션들을 관찰한다
+// 3. 보여지는 섹션에 해당하는 메뉴아이템을 활성화 시킨다
+
+const sectionIds = [
+  ".home",
+  ".about",
+  ".skill",
+  ".work",
+  ".testimonials",
+  ".contact",
+];
+
+const sections = sectionIds.map((id) => document.querySelector(id));
+const navItems = sectionIds.map((id) =>
+  document.querySelector(`[data-link="${id}"]`)
+);
+
+let selectedNavItem = navItems[0];
+
+const observerOptions = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.3,
+};
+const observerCallback = (entries, observer) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting && entry.IntersectionRatio > 0) {
+      const index = sectionIds.indexOf(`#${entry.target.class}`);
+      let selectedIndex;
+      // 스크롤링이 아래로 되어서 페이지가 올라옴
+      if (entry.boundingClient.y < 0) {
+        selectedIndex = index + 1;
+      } else {
+        selectedIndex = index - 1;
+      }
+      selectedNavItem.classList.remove("active");
+      selectedNavItem = navItem(selectedIndex);
+      selectedNavItem.classList.add("active");
+    }
+  });
+};
+
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+sections.forEach((section) => observer.observe(section));
